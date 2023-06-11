377a3e873db2f8d1d0989b728fcf6fa7fec729b2
lyrica <lyricatv@gmail.com>
2023-06-11 21:33:19 +0200

    Install styled-components and create a simple navigation component

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/*
+ * Styling
+*/
+const Container = styled.div`
+    height: 20px;
+    background: blanchedalmond;
+`;
+
+/*
+ * Component
+*/
+const Navigation = () => {
+    return (
+        <Container>
+            This is the navigation
+        </Container>
+    )
+}
+
+export default Navigation;
